906abdfc1c29244eb4badaeecf8fe15291bca511
Josiel <jdlopez114@gmail.com>
2022-11-27 14:25:38 -0500

    header added - Internet Anime DataBase

--- phase-3-project-client/src/components/App.js
+++ phase-3-project-client/src/components/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Header/>
     </div>
   );
-}
+} 
 
 export default App;
---
+++ phase-3-project-client/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header><strong>IADB</strong></header>
+  )
+}
+
+export default Header
