050714cf5e3dd61ad46be6beb648066bcf12ad7f
Josiel <jdlopez114@gmail.com>
2023-01-19 00:21:02 -0500

    removed usestate and useeffect in reviewpage

--- phase-3-project-client/src/components/ReviewPage.js
+++ phase-3-project-client/src/components/ReviewPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import ReviewForm from './ReviewForm';
 import ReviewRow from './ReviewRow';
